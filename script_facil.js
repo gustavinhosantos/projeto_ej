04629cb43056f1ceb8d6ade5d9ef016dba1ec2f6
gusanto <gustavo.s.goncalves@unesp.br>
2023-04-24 13:14:35 -0300

    second commit

--- script_facil.js
+++ script_facil.js
@@ -5,11 +5,11 @@ const espacos = document.querySelectorAll(".espaco");
 let jogador = "X";
 let computador = "O";
 let acabou = false;
-let empatou = false;
+// let empatou = false;
 let jogadas = 0;
+//let disponiveis = [0,1,2,3,4,5,6,7,8];
 
-
-jogadajogador();
+jogada();
 
 
 function ganhou(){
@@ -32,6 +32,7 @@ function ganhou(){
         espacos[combo[2]].textContent === jogador){
             acabou = true;
             alert(`Jogador ${jogador} venceu!`);
+            //ir para a tela03.html
             break;
 
         }if (
@@ -40,22 +41,23 @@ function ganhou(){
         espacos[combo[2]].textContent === computador){
             acabou = true;
             alert(`Jogador ${computador} venceu!`);
+            //ir para a tela03.html
             break;
         }
-        empate();
+        /*
+        if(jogadas === 8 && !acabou){
+            alert("empatou!");
+            //ir para tela03.html
+            break;
+        }
+        */
+
     }
 }
 
-function empate(){
-    if (acabou !== true){
-        if(jogadas === 8){
-            alert("empatou");
-            empatou = true;
-        }
-    }
-}
 
 function jogadacomputador(){
+
     while(true){
         aux = Math.floor(Math.random() * 10);
         if(!acabou && espacos[aux].textContent === ""){
@@ -67,7 +69,7 @@ function jogadacomputador(){
     }
 }
 
-function jogadajogador(){
+function jogada(){
     for (let i = 0; i < espacos.length; i++){
        espacos[i].addEventListener("click", function() {
             if (!acabou && this.textContent === "") {
@@ -75,9 +77,8 @@ function jogadajogador(){
                 ganhou();
                 jogadas++;
                 jogadacomputador();
+                ganhou();
         }
     });
     }
 }
-
-
